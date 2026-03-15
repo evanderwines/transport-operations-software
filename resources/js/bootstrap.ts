52ac8319864541ed64ca27379f2a4cf2a476d57b
Samanseso <winesevander@gmail.com>
2026-03-15 20:37:17 +0800

    update

--- resources/js/bootstrap.ts
+++ resources/js/bootstrap.ts
@@ -17,10 +17,16 @@ const reverbPort = 443;
 window.Echo = new Echo({
   broadcaster: 'reverb',
   key: reverbKey,
+  host: reverbHost,
+  httpHost: reverbHost,
   wsHost: reverbHost,
+  wsPath: '',
   wsPort: reverbPort,
   wssPort: reverbPort,
+  httpPort: reverbPort,
+  httpsPort: reverbPort,
   forceTLS: true,
+  useTLS: true,
   enabledTransports: ['wss'],
   // This prevents the sockjs fallback:
   disableStats: true,
