fa391981841d3ea006640f41399d650e302c71cd
Samanseso <winesevander@gmail.com>
2026-03-17 13:53:57 +0800

    update

--- app/Http/Middleware/AttachTokenFromCookie.php
+++ app/Http/Middleware/AttachTokenFromCookie.php
@@ -4,7 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Laravel\Reverb\Loggers\Log;
+use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\Response;
 
 class AttachTokenFromCookie
